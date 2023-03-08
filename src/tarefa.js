0aca11e83a9ae8b4d0155a4899972594d69a1db1
João Pedro <11joao44@gmail.com>
2023-03-08 17:49:38 -0400

    commit initial

---
+++ src/tarefa.js
@@ -0,0 +1,21 @@
+"use strict";
+// Criando função de multiplicar dois números e retornar a multiplicação deles no caso da minha tarefa ele subtrai mas a logica segue igual.
+function aplicarDesconto(celular, porcentagemDesconto) {
+    const decimal = porcentagemDesconto / 100;
+    const desconto = celular * decimal;
+    const precoComDesconto = celular - desconto;
+    return precoComDesconto;
+}
+const celular = 8999;
+const porcentagemDesconto = 30;
+const precoComDesconto = aplicarDesconto(celular, porcentagemDesconto);
+console.log(`O celular custava R$${celular}, mas com desconto de ${porcentagemDesconto}% agora esta custando R$${precoComDesconto}`);
+// Criando função de saudação que deverá receber um nome e retornar a concatenação
+function saudar(nome) {
+    const saudacoes = ["Olá", "Oi", "Oi tudo bem?", "Oi, como vai?"];
+    const saudacaoAleatoria = saudacoes[Math.floor(Math.random() * saudacoes.length)];
+    return `${saudacaoAleatoria} ${nome} Seja bem-vindo(a)!`;
+}
+const nome = "João";
+const mensagemDeSaudacao = saudar(nome);
+console.log(mensagemDeSaudacao);
